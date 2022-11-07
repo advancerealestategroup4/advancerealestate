257d86f02bf0c693568427a002e5d585fb938e3f
davevanvleet <dvanvleet@unomaha.edu>
2022-11-07 17:20:32 -0600

    Fourth Commit

--- advancerealestate/settings.py
+++ advancerealestate/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = '/home/advancerealestategroup4/advancerealestate/advancerealestate'
+STATIC_ROOT = '/home/advancerealestategroup4/advancerealestate/advancerealestate/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
