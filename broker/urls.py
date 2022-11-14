285fe13287fa899da85561ce2600a0706c1392bd
Dorcas97 <98547266+Dorcas97@users.noreply.github.com>
2022-11-14 00:39:59 -0600

    commit 2

--- broker/urls.py
+++ broker/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('browse/', views.browse, name='browse'),
     path('fun/', views.fun, name='fun'),
 ]
--- broker/views.py
+++ broker/views.py
@@ -4,9 +4,18 @@ from .models import *
 
 
 def index(request):
-    return render(request, "broker/index.html", {'title': 'Home page'})
+    return render(request, "broker/index.html")
+
+
+def about(request):
+    context = {'contacts': Contact.objects.all()}
+    return render(request, "broker/about.html", context)
+
+
+def browse(request):
+    return render(request, "broker/browse.html")
 
 
 def fun(request):
-    return render(request, "broker/fun.html", {'title': 'Fun page'})
+    return render(request, "broker/fun.html")
 
