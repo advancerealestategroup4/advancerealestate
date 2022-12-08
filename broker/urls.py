b69664c9bba241e09f3f3485df937eae5812a4a4
anguyen2396 <77310745+anguyen2396@users.noreply.github.com>
2022-12-08 17:25:36 -0600

    Contact broker form

--- broker/urls.py
+++ broker/urls.py
@@ -1,12 +1,16 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from .import views
 from .views import PropertyListView, PropertyDetailView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-   # path('browse/', views.browse, name='browse'),
+    # path('filter/', views.browse, name='browse'),
     path('browse/', PropertyListView.as_view(), name='browse'),
     path(r'^details/(?P<pk>[0-9]+)\\Z', PropertyDetailView.as_view(), name='details'),
     path('fun/', views.fun, name='fun'),
+
+
+
 ]
